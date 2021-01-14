c3fdf7a5b82e87b65bf5e6c9f065b13c3c20e620
annoniyous <bouysantiman.y@outlook.com>
2021-01-14 10:40:50 +0100

    Fin exercice 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
 import './App.css';
+import navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
-    <img src="./loup bg 2.jpg" alt=""/>
+      <img src="./loup bg 2.jpg" alt=""/>
+      <div>
+        <h1>Ecercices 3</h1>
+        <img src="./loup bg 3.jpg" alt=""/>
+      </div>
      
+      
+      <Navbar/>
     </div>
+    
   );
 }
+
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class navbar extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>home</li>
+                    <li>start</li>
+                    <li>begin</li>
+                </ul>
+                
+            </div>
+        )
+    }
+}
+
+export default navbar
